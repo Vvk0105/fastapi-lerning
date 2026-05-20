4acd3e3cebea5c94b2784f34fc905972571d90ed
Vvk0105 <vivekbabu0105@gmail.com>
2026-05-21 00:32:28 +0530

    post todo done

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from fastapi import FastAPI
-from database import engine
-from models import Base
+from fastapi import FastAPI, Depends
+from database import engine, SessionLocal
+from models import Base, Todo
+from schemas import TodoCreate
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 def home():
     return {"message": "hlo world"}
+
+@app.post("/todos")
+def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
+    new_todo = Todo(title=todo.title)
+
+    db.add(new_todo)
+    db.commit()
+    db.refresh(new_todo)
+
+    return {
+        "message": "Todo Created",
+        "data": {
+            "id": new_todo.id,
+            "title": new_todo.title
+        }
+    }
