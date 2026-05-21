44dbf3f87db6ab04fce44e535ebc7a4f4cad087d
Vvk0105 <vivekbabu0105@gmail.com>
2026-05-21 21:55:45 +0530

    added env install dotenv and use in db congif

--- database.py
+++ database.py
@@ -1,10 +1,23 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import  sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:user123@localhost/todo_db"
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False, 
+    autoflush=False, 
+    bind=engine
+)
 
 Base = declarative_base()
